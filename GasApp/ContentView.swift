c329c3ecb98e4931c9a8aa9101e1856029a4fd7b
najaiscoming <sarannoyprecha@gmail.com>
2022-05-05 19:16:52 +0700

    Initial Commit

---
+++ GasApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GasApp
+//
+//  Created by Saran Noyprecha on 5/5/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GasApp/GasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasAppApp.swift
+//  GasApp
+//
+//  Created by Saran Noyprecha on 5/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct GasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
